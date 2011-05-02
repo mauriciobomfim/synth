64975fdc43715e12586feb70b0a7a43a70a6b3f2
Mauricio Henrique Bomfim <mauriciobomfim@gmail.com>
2011-05-02 05:36:05 +0000

    - Added VOID classes as metaclasses - Added self.subclass to get subclasses of a class

--- app/models/rdfs/class.rb
+++ app/models/rdfs/class.rb
@@ -6,17 +6,21 @@ class RDFS::Class
   end
   
   def self.domain_classes(options={})
-    excluded_namespaces = [:xsd, :rdf, :rdfs, :owl, :shdm, :swui, :symph]
+    excluded_namespaces = [:xsd, :rdf, :rdfs, :owl, :shdm, :swui, :symph, :void]
     RDFS::Class.find_all(options).reject{ |c| excluded_namespaces.include?(ActiveRDF::Namespace.prefix(c))  }
   end
   
   def self.meta_classes(options={})
-    included_namespaces = [:rdf, :rdfs, :owl, :shdm, :swui, :symph]
+    included_namespaces = [:rdf, :rdfs, :owl, :shdm, :swui, :symph, :void]
     RDFS::Class.find_all(options).select{ |c| included_namespaces.include?(ActiveRDF::Namespace.prefix(c))  }
   end
   
   def alpha(property=RDFS::label)
     ActiveRDF::Query.new.distinct(:s).where(:s,RDF::type,self).sort(property).execute
   end
+  
+  def self.subclasses
+    ActiveRDF::Query.new.distinct(:s).where(:s, RDFS::subClassOf, self).execute
+  end  
   
 end
