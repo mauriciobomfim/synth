1b6a775e2e0e0522edf11c42d1729e82747dbed9
Mauricio Henrique Bomfim <mauriciobomfim@gmail.com>
2011-06-20 03:25:26 +0000

    Invalidating caches when create/destroy namespaces.

--- app/controllers/namespaces_controller.rb
+++ app/controllers/namespaces_controller.rb
@@ -17,19 +17,22 @@ class NamespacesController < ApplicationController
   # POST /namespaces.xml
   def create
     @namespace = ActiveRDF::Namespace.register(params[:namespace][:prefix].to_sym, params[:namespace][:uri])
-    
+    ActiveRDF::FederationManager.invalidates_cache
+    $page_cache.clear unless $page_cache.nil?
     respond_to do |format|
       flash[:notice] = 'Namespace was successfully created.'
       format.html { redirect_to :action => :index }
       format.xml  { render :xml => @namespace, :status => :created, :location => @namespace }
     end
+    
   end
 
   # DELETE /namespaces/1
   # DELETE /namespaces/1.xml
   def destroy
     @namespace = ActiveRDF::Namespace.remove(params[:id].to_sym)
-     
+    ActiveRDF::FederationManager.invalidates_cache
+    $page_cache.clear unless $page_cache.nil?
     respond_to do |format|
       format.html { redirect_to :action => :index }
       format.xml  { head :ok }
